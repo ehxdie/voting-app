67f2c08a22a1815ed7495df83a89cf2c5500526a
edidiong <ephraimedy@gmail.com>
2023-03-31 23:57:27 +0100

    Added candidate information to the contract page

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -1,34 +1,24 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.0;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
 
-contract Lock {
-    uint public unlockTime;
-    address payable public owner;
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "hardhat/console.sol";
 
-    event Withdrawal(uint amount, uint when);
+contract Create {
+   // In order to keep record of voters
+   using Counters for Counters.Counter;
+   Counters.Counter public voterID;
 
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
 
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
-    }
-
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
-
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
-
-        emit Withdrawal(address(this).balance, block.timestamp);
-
-        owner.transfer(address(this).balance);
-    }
+   // Candidate information
+   struct Candidate {
+      uint256 candidateID;
+      string age;
+      string name;
+      uint256 votecount;
+      address _address;
+   }  
+   
+ 
 }
