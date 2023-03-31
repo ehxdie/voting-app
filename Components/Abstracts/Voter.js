42de8c900c26e0133cbd600c0619d90fb3c11b2a
edidiong <ephraimedy@gmail.com>
2023-03-31 22:34:53 +0100

    Added pages that allow for communicating with the smart contract

---
+++ Components/Abstracts/Voter.js
@@ -0,0 +1,2 @@
+// File that handles api calls to the smart contract
+// Ideally in the enclave of an Sgx
---
+++ src/AllowedVoters.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function AllowedVoters() {
+  return (
+    <div>allowed-voters</div>
+  )
+}
+
+export default AllowedVoters
